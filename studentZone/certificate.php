e3e0e2f03f6463af2434b7c184adbd9cc4928cab
unknown <ankitbkana@outlook.com>
2023-11-02 23:36:33 +0530

    certificate edited

--- studentZone/certificate.php
+++ studentZone/certificate.php
@@ -1,6 +1,11 @@
 <div class="certificate-con">
     <div class="certificate">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut modi, dicta sed ullam molestiae est corrupti iure similique numquam dolorem officiis laboriosam deserunt corporis, quaerat consequatur nisi fugit unde odit.
-        <!-- <img src="certificate\certificatebg.jpg" alt=""> -->
+        <div class="certificate-data">
+            <p class="student-data">This is certified that Mr <b>Ankit</b> has successfuly completed their <b>web develpment</b> course from 1/6/2023 to 1/9/2023. he is very honesty and hardworking person. he clear their all test and we wish their bright future  </p>
+        </div>
+        <p class="certificate-date">1/11/2023</p>
+        <div class="certificate-bg">
+            <img src="certificate\certificatebg.jpg" alt="">
+        </div>
     </div>
 </div>
